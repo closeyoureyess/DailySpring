5161a46c7197f42712d59157f0601870c988e1b2
mxnv <vadim.beller@mail.ru>
2024-06-29 17:05:59 +0300

    master - final version

--- myProject/src/main/java/MyController.java
+++
@@ -1,8 +0,0 @@
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class MyController {
-    public void contr(){
-        System.out.println("Контроллер");
-    }
-}
--- myProject/src/main/java/MyService.java
+++
@@ -1,8 +0,0 @@
-import org.springframework.stereotype.Service;
-
-@Service
-public class MyService {
-    public void ser(){
-        System.out.println("Hey!");
-    }
-}
