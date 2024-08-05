22021cf7c405c5249455bdd4d54134db403e8b06
mxnv <vadim.beller@mail.ru>
2024-08-05 14:00:44 +0300

    master

--- myProject/src/main/java/com/example/myProject/config/KafkaConsumerConfig.java
+++ myProject/src/main/java/com/example/myProject/config/KafkaConsumerConfig.java
@@ -22,23 +22,35 @@ public class KafkaConsumerConfig {
     @Value("${spring.kafka.consumer.bootstrap-servers}")
     private String bootStrapServers;
 
-    @Bean
-    public ConsumerFactory<String, CaseDto> consumerConfigs() {
+    /*public Map<String, Object> consumerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        JsonDeserializer<CaseDto> deserializer = new JsonDeserializer<>(CaseDto.class);
+        *//*JsonDeserializer<CaseDto> deserializer = new JsonDeserializer<>(CaseDto.class);
         deserializer.setRemoveTypeHeaders(false);
         deserializer.addTrustedPackages("*");
-        deserializer.setUseTypeMapperForKey(true);
+        deserializer.setUseTypeMapperForKey(true);*//*
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
-        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        return props;
+    }*/
+
+    @Bean
+    public ConsumerFactory<String, CaseDto> userConsumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServers);
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(),
+                new JsonDeserializer<>(CaseDto.class));
     }
 
     @Bean
-    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, CaseDto>> kafkaListenerContainerFactory(){
-        ConcurrentKafkaListenerContainerFactory<String, CaseDto> consFactory = new ConcurrentKafkaListenerContainerFactory<>();
-        consFactory.setConsumerFactory(consumerConfigs());
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, CaseDto>>
+    kafkaListenerContainerFactory(){
+        ConcurrentKafkaListenerContainerFactory<String, CaseDto> consFactory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        consFactory.setConsumerFactory(userConsumerFactory());
         return consFactory;
     }
 }
