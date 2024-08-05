d0dc93bfcd44ef2fc31c5ea35788dbcf27b2049e
mxnv <vadim.beller@mail.ru>
2024-08-05 17:08:02 +0300

    master

--- myProject/src/main/java/com/example/myProject/CaseRepository.java
+++ myProject/src/main/java/com/example/myProject/CaseRepository.java
@@ -1,6 +1,8 @@
 package com.example.myProject;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CaseRepository extends JpaRepository<Case, Integer> {
 }
